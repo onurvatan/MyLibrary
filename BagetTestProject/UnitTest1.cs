4e586ae50ac73e92dc8bbf81facb09c447471db7
onrvtn <onur.vatan@his.com.tr>
2019-05-07 18:00:44 +0300

    Add project files.

---
+++ BagetTestProject/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BagetTestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            MyLibrary.Class1 class1 = new MyLibrary.Class1();
+        }
+    }
+}
